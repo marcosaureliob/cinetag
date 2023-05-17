03add1424ed29613b320bac3f109a8955f4e6270
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-05-17 09:38:44 -0300

    Ajustes na pagina de favorito

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,21 +1,24 @@
-import Banner from 'components/Banner';
-import Card from 'components/Card';
-import Titulo from 'components/Titulo';
-import styles from './Favoritos.module.css';
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
+import styles from "./Favoritos.module.css";
+import { useFavoritoContext } from "contextos/Favoritos";
 
 function Favoritos() {
-    return (
-        <>
-            <Banner imagem='favoritos' />
-            <Titulo>
-                <h1>Meus Favoritos</h1>
-            </Titulo>
-            <section className={styles.container}>
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
-            </section>
-
-        </>
-    )
+  const { favorito } = useFavoritoContext();
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Favoritos;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Cabecalho from "components/Cabecalho";
 import Container from "components/Container";
 import Rodape from "components/Rodape";
+import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -8,14 +9,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function AppRoutes() {
   return (
     <BrowserRouter>
-    <Cabecalho/>
-    <Container>
-      <Routes>
-        <Route path="/" element={<Inicio />}></Route>
-        <Route path="/favoritos" element={<Favoritos />}></Route>
-      </Routes>
+      <Cabecalho />
+      <Container>
+        <FavoritosProvider>
+          <Routes>
+            <Route path="/" element={<Inicio />}></Route>
+            <Route path="/favoritos" element={<Favoritos />}></Route>
+          </Routes>
+        </FavoritosProvider>
       </Container>
-      <Rodape/>
+      <Rodape />
     </BrowserRouter>
   );
 }
