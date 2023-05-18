b6f96c3e570526f2151d2625ac760de80bfdf381
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-05-18 15:25:15 -0300

    projeto final

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,23 +1,33 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from "json/db.json";
-import styles from "./Inicio.module.css";
+import { useEffect, useState } from "react";
+import styles from './Inicio.module.css';
 
 function Inicio() {
-  return (
-    <>
-      <Banner imagem="home" />
-      <Titulo>
-        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
-      </Titulo>
-      <section className={styles.container}>
-        {videos.map((video) => {
-          return <Card {...video} key={video.id} />;
-        })}
-      </section>
-    </>
-  );
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/monicahillman/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
+    return (
+        <>
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
+    )
 }
 
 export default Inicio;
